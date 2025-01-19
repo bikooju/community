0aa8670189f93653ef041808b56482f4275af8b5
주현준 <jhjk1234@gmail.com>
2025-01-19 16:59:49 +0900

    UserRepository 수정

--- src/main/java/com/exchangeBE/exchange/repository/UserRepository.java
+++ src/main/java/com/exchangeBE/exchange/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // 사용자 이름으로 사용자 조회
     Optional<User> findByUsername(String username);
